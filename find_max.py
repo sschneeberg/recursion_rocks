64f51ed22a57d22503903d7eaa04e3a2dc9e72ea
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-10 13:59:23 -0500

    find max

--- find_max.py
+++ find_max.py
@@ -4,8 +4,13 @@
 # This function returns the largest number in a given array.
 
 def find_max(l):
-    # Write code here
-    pass
+    if type(l[0]) != int: raise ValueError("l must be a list of integers")
+    if len(l) <= 1: return 1
+    else: return max(l[0], find_max(l[1:]))
 
 # print(find_max([1, 4, 45, 6, -50, 10, 2]))
 # => 45
+
+assert(find_max([1, 4, 45, 6, -50, 10, 2]) == 45)
+assert(find_max([3,4,-5,7,23,89,23542345,85,3234,43]) == 23542345)
+# assert(find_max([1,5,'a']) == 0) #should raise error
